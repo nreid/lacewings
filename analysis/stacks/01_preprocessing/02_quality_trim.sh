e04142045c5e22b4e3c1008c4c6b573a834de6e6
Noah Reid <noah.reid@uconn.edu>
2019-11-06 14:02:23 -0500

    trimmomatic

--- analysis/stacks/01_preprocessing/02_quality_trim.sh
+++ analysis/stacks/01_preprocessing/02_quality_trim.sh
@@ -5,11 +5,11 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=noah.reid@uconn.edu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mem=20G
 #SBATCH --partition=general
 #SBATCH --qos=general
-#SBATCH --array=[0-140]
+#SBATCH --array=[0-140]%20
 
 module load java
 module load Trimmomatic/0.36
@@ -22,17 +22,16 @@ echo This is array task number $SLURM_ARRAY_TASK_ID
 ADAPT=../../../metadata/TruSeq3-SE.fa
 
 INDIR=../results/demultiplexed_fastqs
-POOLS=($(ls -1 $INDIR/*fastq.gz))
-INFILE=$(echo ${POOLS[$SLURM_ARRAY_TASK_ID]} | sed 's/.*\///')
+FASTQS=($(ls -1 $INDIR/*fastq.gz))
+INFILE=$(echo ${FASTQS[$SLURM_ARRAY_TASK_ID]} | sed 's/.*\///')
 OUTFILE=$(echo $INFILE | sed 's/fastq/trim.fastq/')
 
 # make trimmed directory if it doesn't exist
 mkdir -p ../results/trimmed_data
 OUTDIR=../results/trimmed_data
 
-
 java -jar $Trimmomatic SE \
--threads 20 \
+-threads 10 \
 $INDIR/$INFILE \
 $OUTDIR/$OUTFILE \
 ILLUMINACLIP:$ADAPT:2:30:10 \
