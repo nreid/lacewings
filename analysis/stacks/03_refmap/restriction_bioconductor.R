b6dbe7435d933f1bd893b07ab92989c5ecb4c5c7
Noah Reid <noah.reid@uconn.edu>
2019-11-12 15:26:47 -0500

    targets

--- analysis/stacks/03_refmap/restriction_bioconductor.R
+++ analysis/stacks/03_refmap/restriction_bioconductor.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(Biostrings)     # Provides DNAString, DNAStringSet, etc
 library(BSgenome)       # Provides getSeq()
 library(GenomicRanges)  # Provides GRanges, etc
@@ -6,25 +7,54 @@ library(rtracklayer)    # Provides import() and export()
 
 # This R script finds sbf1 cut sites, flanking sites and writes them to bed files
 
+# read in reference genome 
 seqs <- readDNAStringSet("../results/redundans_metaquast_filtered.nomt.masked.fasta")
 
+# find sbf1 sites
 mp <- vmatchPattern("CCTGCAGG",seqs)
 
+# make it a GRanges object
 mp2 <- as(mp, "GRanges")
 
+# make it a data frame
 mp3 <- as.data.frame(mp2)
 
+# 0-index start
 mp3[,2] <- mp3[,2] - 1
 
+# left flank
 left <- mp3
 	left[,2] <- left[,2] - 9
 	left[,3] <- left[,3] - 9
 left[left[,2] < 0,2] <- 0
 
+# right flank
 right <- mp3
 	right[,2] <- right[,2] + 9
 	right[,3] <- right[,3] + 9
 
+# find sbf1 1-off sites
+mp_1 <- vmatchPattern("CCTGCAGG",seqs,max.mismatch=1)
+
+# make it a GRanges object
+mp2_1 <- as(mp_1, "GRanges")
+
+# make it a data frame
+mp3_1 <- as.data.frame(mp2_1)
+
+# 0-index start
+mp3_1[,2] <- mp3_1[,2] - 1
+
+
+# write tables
 write.table(mp3[,1:3],"../../../metadata/sbf1.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
+write.table(mp3_1[,1:3],"../../../metadata/sbf1_off.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
 write.table(left[,1:3],"../../../metadata/sbf1_left.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
 write.table(right[,1:3],"../../../metadata/sbf1_right.bed",sep="\t",row.names=FALSE,col.names=FALSE,quote=FALSE)
+
+# There are 24 possible off-by-1 cut sites. 
+
+cuts <- getSeq(seqs,mp2_1)
+
+as.character(cuts) %>% table() %>% sort()
+
---
+++ analysis/stacks/03_refmap/targets.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(GenomicRanges)
+library(beeswarm)
+
+
+# This script examines coverage among individuals for intervals
+# identified as having substantial sequencing coverage
+# It writes out a set of target regions for variant calling. 
+
+# genomic intervals with coverage
+pr <- read.table("possibleradsites_counts.txt",stringsAsFactors=FALSE,header=TRUE)
+colnames(pr)[-c(1:6)] <- str_extract(colnames(pr)[-c(1:6)],regex("[0-9]+[^.]+"))
+
+# sbf1 cut sites
+sbf1 <- read.table("../../../../metadata/sbf1.bed",stringsAsFactors=FALSE)
+
+# sbf1 + 1-off cut sites
+sbf1off <- read.table("../../../../metadata/sbf1.bed",stringsAsFactors=FALSE)
+
+
+# generate Granges objects
+prg <- GRanges(
+	seqnames = pr[,2],
+	ranges = IRanges(start = pr[,3], end = pr[,4])
+	)
+
+sbf1g <- GRanges(
+	seqnames = sbf1[,1],
+	ranges = IRanges(start = sbf1[,2]+1, end = sbf1[,3])
+	)
+
+sbf1offg <- GRanges(
+	seqnames = sbf1off[,1],
+	ranges = IRanges(start = sbf1off[,2]+1, end = sbf1off[,3])
+	)
+
+
+# get overlaps between coverage intervals and cut sites
+olaps <- findOverlaps(prg,sbf1g) %>% as.matrix()
+olapsoff <- findOverlaps(prg,sbf1offg) %>% as.matrix()
+
+# species names
+spec <- gsub("^....","",colnames(pr)[-c(1:6)])
+
+# vector of intervals with cut sites
+rr <- rep(FALSE,nrow(pr))
+rr[olaps[,1]] <- TRUE
+
+# and 1-off cut sites. 
+rroff <- rep(FALSE,nrow(pr))
+rroff[olapsoff[,1]] <- TRUE
+
+# missing data and read counts across individuals
+md <- rowSums(pr[,-c(1:6)] > 0)
+co <- rowSums(pr[,-c(1:6)])
+
+# subset the sites
+keep <- md > 20 & co < 8e5 & co > 3000
+pr2 <- pr[keep,]
+rr2 <- rr[keep]
+rroff2 <- rroff[keep]
+md2 <- md[keep]
+
+# vector to exclude low cov individuals
+colMeans(pr2s[md2 > 100,]>0) %>% plot()
+ind <- colMeans(pr2s[md2 > 100,]>0) > 0.5
+
+# scaled counts
+pr2s <- apply(pr2[,-c(1:6)],MAR=2,FUN=function(x){x/sum(x)*1e6})
+
+# plot missing data as a function of total coverage
+plot(log(rowSums(pr2s),10),rowSums(pr2s>0),col=rr2+rroff2+1)
+
+# plot missing data as a function of upper -tile coverage
+tile <- apply(pr2s[,ind],MAR=1,FUN=quantile,prob=0.88)
+plot(log(tile,10),rowSums(pr2s>0),col=rr2+rroff2+1,pch=20,cex=0.4)
+
+# beeswarm as a function of no cut site (1), 1-off (2) and cut site (3)
+cutstatus <- rr2+rroff2+1
+beeswarm(rowSums(pr2s[,ind]>0) ~ cutstatus,pch=20,cex=.5)
+beeswarm(log(tile,10) ~ cutstatus,pch=20,cex=.5)
+
+# write out target regions
+
+targets <- cbind(scaf=pr2[,2],start=pr2[,3]-1,end=pr2[,4])
+
+
+
+
+
