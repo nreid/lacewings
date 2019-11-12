974f31dab8ffa9456f271e73b0111746298d1f98
Noah Reid <noah.reid@uconn.edu>
2019-11-12 15:49:53 -0500

    freebayes

---
+++ analysis/stacks/04_freebayes/01_freebayes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+#SBATCH --job-name=freebayes
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 2
+#SBATCH --mem=50G
+#SBATCH --qos=general
+#SBATCH --partition=general
+
+
+hostname
+date
+
+# load software
+module load freebayes/1.1.0
+module load htslib/1.9
+
+
+# input/output directories, supplemental files
+
+INDIR=../results/trimmed_aligned
+
+# bam list
+find $INDIR -name "*bam" | sort >$INDIR/bams.list
+BAMLIST=$INDIR/bams.list
+
+OUTDIR=../results/freebayes
+mkdir -p $OUTDIR
+
+# ref genome
+REFERENCE=../results/redundans_metaquast_filtered.nomt.masked.fasta
+
+# set a variable for the sites targeted
+TARGETS=../../../metadata/targets.bed
+
+freebayes -f $GEN -L $BAMLIST -t $TARGETS -m 30 -q 20 -k -V | \
+bgzip -c >$OUTDIR/chrysoperla.vcf.gz
+
+tabix -p vcf $OUTDIR/chrysoperla.vcf.gz
---
+++ analysis/stacks/04_freebayes/README.md
@@ -0,0 +1,4 @@
+# Freebayes analysis
+
+- uses prior preprocessing steps from 01_preprocessing and 03_refmap
+- 
