cb45ebe07991e5a951748c172cdebb0c1a1988b2
Noah Reid <noah.reid@uconn.edu>
2019-11-19 10:23:58 -0500

    populations

---
+++ analysis/stacks/02_denovo/06_populations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH --job-name=populations
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 8
+#SBATCH --mem=15G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+
+INDIR=../results/denovo_ustacks
+
+POPMAP=../../../metadata/popmap.txt
+
+populations -P $INDIR -M $POPMAP -r 0.65 --vcf --fasta-samples --treemix -t 8
