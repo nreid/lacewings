6c6bfe1fef94e9900dfa65792046c536afab592d
Noah Reid <noah.reid@uconn.edu>
2019-11-26 12:55:27 -0500

    bcftools

---
+++ analysis/stacks/calocedrii_test/08_bcftools.sh
@@ -0,0 +1,50 @@
+#!/bin/bash 
+#SBATCH --job-name=bcftools
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+
+hostname
+date
+
+# load software
+module load bcftools/1.9
+module load htslib/1.9
+
+# input/output directories, supplemental files
+
+INDIR=../results/trimmed_aligned
+
+# bam list
+find $INDIR -name "*calocedrii*bam" | sort >$INDIR/calocedrii_bams.list
+BAMLIST=$INDIR/bams.list
+
+OUTDIR=../results/calocedrii_test
+mkdir -p $OUTDIR
+
+# ref genome
+REFERENCE=../results/redundans_metaquast_filtered.nomt.masked.fasta
+
+# set a variable for the sites targeted
+TARGETS=../../../metadata/targets.bed
+
+
+bcftools mpileup \
+	-f $REFERENCE \
+	-b $BAMLIST \
+	-q 20 -Q 30 \
+	--max-depth 10000 \
+	-a "DP,AD" \
+	--targets-file $TARGETS | \
+bcftools call -m -v -Oz -o $OUTDIR/calocedrii.vcf.gz
+
+
+
