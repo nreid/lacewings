9f16ee291441f94c07e612fed511383b78b666c0
Noah Reid <noah.reid@uconn.edu>
2019-12-10 12:53:47 -0500

    bwa

---
+++ tutorial/scripts/stacks/11_bwa.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=bwa
+#SBATCH -o %x_%A_%a.out
+#SBATCH -e %x_%A_%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=5G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --array=[0-32]%20
+
+
+hostname
+date
+
+# load software
+module load bwa/0.7.17
+module load samtools/1.9
+
+# input, output files and directories
+INDIR=../../results/trimmed_data
+
+OUTDIR=../../results/aligned
+mkdir -p $OUTDIR
+
+# indexed reference genome
+REFERENCE=../../genome/redundans_metaquast_filtered.nomt.masked.fasta
+
+FASTQS=($(ls -1 $INDIR/*.fastq.gz))
+
+INFILE=$(echo ${FASTQS[$SLURM_ARRAY_TASK_ID]} | sed 's/.*\///')
+OUTFILE=$(echo $INFILE | sed 's/fastq.gz/bam/')
+
+SAM=$(echo $OUTFILE | sed 's/\..*//')
+RG=$(echo \@RG\\tID:$SAM\\tSM:$SAM)
+
+bwa mem -t 4 -R $RG $REFERENCE $INDIR/$INFILE | \
+samtools view -S -h -u - | \
+samtools sort -T /scratch/$SAM - >$OUTDIR/$OUTFILE
+
+date
---
+++ tutorial/scripts/stacks/12_refmapPL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+#SBATCH --job-name=refmap.pl
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 10
+#SBATCH --mem=30G
+#SBATCH --qos=general
+#SBATCH --partition=general
+
+
+hostname
+date
+
+# load software
+module load stacks/2.41
+
+# input, output files, directories
+
+INDIR=../../results/aligned
+OUTDIR=../../results/stacks_refmap
+mkdir -p $OUTDIR
+
+# popmap file with .trim suffix
+POPMAP=../../metadata/popmap.txt
+cat $POPMAP | sed 's/\s/.trim\t/' >${POPMAP}.trim
+POPMAP=${POPMAP}.trim
+
+# refmap.pl -s option is broken. 
+ref_map.pl --samples $INDIR --popmap $POPMAP -o $OUTDIR -T 10
