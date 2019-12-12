28acf02f13f88ed463266ee2b48871c1a665e221
Noah Reid <noah.reid@uconn.edu>
2019-12-12 16:14:23 -0500

    tutorial ipyrad

---
+++ tutorial/scripts/ipyrad/02_ipyrad_refmap.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+#SBATCH --job-name=ipyrad_full
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem=50G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+module load ipyrad/0.9.19
+
+
+# create population file:
+
+BC=../../metadata/henrysamples.txt
+BCP=../../metadata/henrysamples_ipyrad.txt
+POP=../../metadata/popmap.ipyrad.txt
+
+paste <(cut -f 2 $BC) <(cut -f 1 $BC) >$BCP
+paste <(cut -f 2 $BC) <(cut -f 2 $BC | sed 's/^....//') >$POP
+cut -f 2 ../../metadata/popmap.txt | sort | uniq | sed 's/$/:0 /' | tr "\n" " " | sed 's/^/# /' >>$POP
+
+ipyrad -p params-fulldata09.txt -s 1 -c 32 -r 
+
+echo step 1 done
+
+ipyrad -p params-fulldata09.txt -s 2 -c 32 -r 
+ 
+echo step 2 done
+
+ipyrad -p params-fulldata09.txt -s 3 -c 32 -r 
+
+echo step 3 done
+
+ipyrad -p params-fulldata09.txt -s 4 -c 32 -r 
+
+echo step 4 done
+
+ipyrad -p params-fulldata09.txt -s 5 -c 32 -r 
+
+echo step 5 done
+
+ipyrad -p params-fulldata09.txt -s 6 -c 32 -d  -r 
+
+echo step 6 done
+
+ipyrad -p params-fulldata09.txt -s 7 -c 32 -d  -r 
+
+echo step 7 done
+
