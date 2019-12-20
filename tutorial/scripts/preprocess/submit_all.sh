5754befe9b552ff5b930ac50252504ee4f905970
Noah Reid <noah.reid@uconn.edu>
2019-12-20 14:26:16 -0500

    submit all

--- tutorial/scripts/preprocess/submit_all.sh
+++ tutorial/scripts/preprocess/submit_all.sh
@@ -2,9 +2,9 @@
 
 # submit all preprocessing steps as dependencies. 
 
-jid1=$(sbatch 01_processradtags.sh)
-jid2=$(sbatch --dependency=afterok:$jid1 02_fastqc.sh)
-jid3=$(sbatch --dependency=afterok:$jid2 03_multiqc.sh)
-jid4=$(sbatch --dependency=afterok:$jid3 04_qualitytrim.sh)
-jid5=$(sbatch --dependency=afterok:$jid4 05_trimfastqc.sh)
-jid6=$(sbatch --dependency=afterok:$jid5 06_trimmultiqc.sh)
+jid1=$(sbatch --parsable 01_processradtags.sh)
+jid2=$(sbatch --parsable --dependency=afterok:$jid1 02_fastqc.sh)
+jid3=$(sbatch --parsable --dependency=afterok:$jid2 03_multiqc.sh)
+jid4=$(sbatch --parsable --dependency=afterok:$jid3 04_qualitytrim.sh)
+jid5=$(sbatch --parsable --dependency=afterok:$jid4 05_trimfastqc.sh)
+jid6=$(sbatch --parsable --dependency=afterok:$jid5 06_trimmultiqc.sh)
